b77b134147cc8a95d42cff329df29250a16aecd0
xesam <xesamguo@gmail.com>
2021-06-18 13:38:38 +0800

    demo fix

--- test/groupchat.list.js
+++ test/groupchat.list.js
@@ -3,6 +3,6 @@ const writeFile = require('write-file-promise');
 const groupchat = require('../src/externalcontact/groupchat');
 
 
-groupchat.list({limit: 300}).then(data => {
+groupchat.list({limit: 500}).then(data => {
     return writeFile('./groupchat.list.json5', json5.stringify(data, null, 4), {encoding: 'utf-8'});
 });
